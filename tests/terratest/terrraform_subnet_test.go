884d5bc5ccfb98ac91d97dfd52784aca925bb17c
saalim.khan <saalim.khan@hmcts.net>
2022-10-20 13:50:10 +0100

    udpated test case

--- tests/terratest/terrraform_subnet_test.go
+++ tests/terratest/terrraform_subnet_test.go
@@ -2,29 +2,55 @@ package test
 
 import (
 	"testing"
-	"github.com/gruntwork-io/terratest/modules/terraform"
-)
 
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/test-structure"
+)
 
 func TestTerraformSubnet(t *testing.T) {
 	t.Parallel()
 
+	fixtureFolder := "./example"
+
+	// Deploy the example
+	test_structure.RunTestStage(t, "setup", func() {
+		terraformOptions := configureTerraformOptions(t, fixtureFolder)
+
+		// Save the options so later test stages can use them
+		test_structure.SaveTerraformOptions(t, fixtureFolder, terraformOptions)
+
+		// This will init and apply the resources and fail the test if there are any errors
+		terraform.InitAndApply(t, terraformOptions)
+	})
+
+	// Check whether the length of output meets the requirement
+	test_structure.RunTestStage(t, "validate", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
+
+		subnetId := terraform.Output(t, terraformOptions, "subnet_id")
+		if len(subnetId) <= 0 {
+			t.Fatal("Wrong output")
+		}
+	})
+
+	// At the end of the test, clean up any resources that were created
+	test_structure.RunTestStage(t, "teardown", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
+		terraform.Destroy(t, terraformOptions)
+	})
+
+}
+
+func configureTerraformOptions(t *testing.T, fixtureFolder string) *terraform.Options {
+
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: "./example",
+		TerraformDir: fixtureFolder,
 		VarFiles: []string{"subnet.tfvars"},
-		Upgrade: true,
+
+		// Variables to pass to our Terraform code using -var options
+		//Vars: map[string]interface{}{},
 	}
 
-	// Defer the destroy to cleanup all created resources
-    defer terraform.Destroy(t, terraformOptions)
-
-	// This will init and apply the resources and fail the test if there are any errors
-	terraform.InitAndApply(t, terraformOptions)
-
-	// Verify configurations
-    string := terraform.Output(t, terraformOptions, "subnet_id")
-	if len(string) <= 0 {
-		t.Fatal("Wrong output")
-	}
+	return terraformOptions
 }
