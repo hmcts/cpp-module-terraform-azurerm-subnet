8dd40ba0ab68de9880b1d2828c449bf9b6d4b58b
Abhivan Chekuri <abhivan.chekuri@hmcts.net>
2022-11-24 16:55:13 +0000

    EI-948 Add option to disable peivate link service network policies
    Change-Id: I0924d48d84d35967e807af32cbfe0f34765b7b41

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name                           = var.virtual_network_name
   address_prefixes                               = var.subnet_address_prefixes
   enforce_private_link_endpoint_network_policies = var.subnet_enforce_private_link_endpoint_network_policies
+  enforce_private_link_service_network_policies  = var.subnet_enforce_private_link_service_network_policies
 
   # service_endpoints    = ["Microsoft.Web","Microsoft.Storage","Microsoft.ContainerRegistry"]
 
