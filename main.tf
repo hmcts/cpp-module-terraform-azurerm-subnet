fe1079ddf90fbdfbf2eef4a23ba77d20447df6e5
Joshua Glass <joshua.glass@justice.gov.uk>
2024-11-04 11:31:52 +0000

    4.x updates

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name                            = var.core_resource_group_name
   virtual_network_name                           = var.virtual_network_name
   address_prefixes                               = var.subnet_address_prefixes
-  enforce_private_link_endpoint_network_policies = var.subnet_enforce_private_link_endpoint_network_policies
-  enforce_private_link_service_network_policies  = var.subnet_enforce_private_link_service_network_policies
+  private_endpoint_network_policies              = var.subnet_enforce_private_link_endpoint_network_policies
+  private_link_service_network_policies_enabled  = var.subnet_enforce_private_link_service_network_policies
 
   # service_endpoints    = ["Microsoft.Web","Microsoft.Storage","Microsoft.ContainerRegistry"]
 
