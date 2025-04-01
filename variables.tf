bd0dc7391e78db125efac57077d0ed591ee428f7
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2025-04-01 20:30:39 +0100

    change false to disabled

--- variables.tf
+++ variables.tf
@@ -30,7 +30,7 @@ variable "subnet_address_prefixes" {
 variable "subnet_enforce_private_link_endpoint_network_policies" {
   description = "subnet endpoint network policy"
   type        = bool
-  default     = false
+  default     = Disabled
 }
 
 variable "subnet_enforce_private_link_service_network_policies" {
