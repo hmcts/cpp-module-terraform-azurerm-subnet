f632bf075fde958431ba6ea79ba596c3e71edc73
Venkata Challa <venkata.challa80@gmail.com>
2025-06-26 22:38:56 +0100

    Added enforce_private_link_service_network_policies

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name                            = var.core_resource_group_name
   virtual_network_name                           = var.virtual_network_name
   address_prefixes                               = var.subnet_address_prefixes
-  enforce_private_link_endpoint_network_policies = var.subnet_enforce_private_link_endpoint_network_policies
+  private_link_service_network_policies_enabled  = false
   private_endpoint_network_policies              = "Disabled"
 
   # service_endpoints    = ["Microsoft.Web","Microsoft.Storage","Microsoft.ContainerRegistry"]
