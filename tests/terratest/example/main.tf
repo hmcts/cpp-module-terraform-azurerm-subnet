bd87c308fdb5fd8374b3e5ddc2e151dce825fd87
Dawid Strozak <dawid.strozak@hmcts.net>
2026-05-21 16:02:06 +0100

    Fix spaces for linting

--- tests/terratest/example/main.tf
+++ tests/terratest/example/main.tf
@@ -26,12 +26,12 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   depends_on          = [azurerm_resource_group.test]
   subnet {
-    name           = "subnet1"
+    name             = "subnet1"
     address_prefixes = ["10.0.1.0/24"]
   }
 
   subnet {
-    name           = "subnet2"
+    name             = "subnet2"
     address_prefixes = ["10.0.2.0/24"]
     #security_group = azurerm_network_security_group.example.id
   }
