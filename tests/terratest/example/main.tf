4c1e099168339bd0f9d9a48e6052b9200d371244
Dawid Strozak <dawid.strozak@hmcts.net>
2026-05-26 14:07:49 +0100

    Update private link policy property in test

--- tests/terratest/example/main.tf
+++ tests/terratest/example/main.tf
@@ -43,6 +43,6 @@ module "subnet" {
   core_resource_group_name                              = azurerm_resource_group.test.name
   virtual_network_name                                  = azurerm_virtual_network.vnet.name
   subnet_address_prefixes                               = ["10.0.10.0/24"]
-  subnet_enforce_private_link_endpoint_network_policies = true
+  private_endpoint_network_policies                     = "Enabled"
   depends_on                                            = [azurerm_virtual_network.vnet]
 }
