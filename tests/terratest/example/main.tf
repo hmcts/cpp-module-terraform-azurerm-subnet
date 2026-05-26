31547b816046daebcfa3b277b9a7dbd4b60b2888
Dawid Strozak <dawid.strozak@hmcts.net>
2026-05-26 14:14:52 +0100

    Fix lint

--- tests/terratest/example/main.tf
+++ tests/terratest/example/main.tf
@@ -38,11 +38,11 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 module "subnet" {
-  source                                                = "../../../"
-  subnet_name                                           = "subnet3"
-  core_resource_group_name                              = azurerm_resource_group.test.name
-  virtual_network_name                                  = azurerm_virtual_network.vnet.name
-  subnet_address_prefixes                               = ["10.0.10.0/24"]
-  private_endpoint_network_policies                     = "Enabled"
-  depends_on                                            = [azurerm_virtual_network.vnet]
+  source                            = "../../../"
+  subnet_name                       = "subnet3"
+  core_resource_group_name          = azurerm_resource_group.test.name
+  virtual_network_name              = azurerm_virtual_network.vnet.name
+  subnet_address_prefixes           = ["10.0.10.0/24"]
+  private_endpoint_network_policies = "Enabled"
+  depends_on                        = [azurerm_virtual_network.vnet]
 }
