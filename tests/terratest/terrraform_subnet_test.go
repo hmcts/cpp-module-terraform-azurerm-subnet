dc4bec298f445b7851181af55fc8b6893500b0a4
Dawid Strozak <dawid.strozak@hmcts.net>
2026-05-26 09:59:08 +0100

    Try commenting out tf import as it seems to already be imported

--- tests/terratest/terrraform_subnet_test.go
+++ tests/terratest/terrraform_subnet_test.go
@@ -3,7 +3,7 @@ package test
 import (
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/terraform"
+	// "github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
