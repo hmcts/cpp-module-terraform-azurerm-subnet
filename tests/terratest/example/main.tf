00959cd1808285b95271420f1b8a74847f813d18
Dawid Strozak <dawid.strozak@hmcts.net>
2026-06-01 12:49:27 +0100

    Fix one more lint issue

--- tests/terratest/example/main.tf
+++ tests/terratest/example/main.tf
@@ -39,8 +39,8 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   depends_on          = [azurerm_resource_group.test]
   subnet {
-    name             = "subnet1"
-    address_prefix   = "10.0.1.0/24"
+    name              = "subnet1"
+    address_prefix    = "10.0.1.0/24"
   }
 
   subnet {
