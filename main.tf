58b0747cdf220d7c3a9bdd95c180596ae6070baa
dawidstrozak <123395719+dawidstrozak@users.noreply.github.com>
2026-06-01 15:23:43 +0100

    Restore deprecated azurerm provider parameters (#13)
    * Restore deprected subnet properties used by older azurerm provider to maintain upstream compatibility
    * Set azuerrm provider version explicitly so pre-commit checks run against the correct version of provider and pass

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 resource "azurerm_subnet" "subnet" {
-  name                                          = var.subnet_name
-  resource_group_name                           = var.core_resource_group_name
-  virtual_network_name                          = var.virtual_network_name
-  address_prefixes                              = var.subnet_address_prefixes
-  private_endpoint_network_policies             = var.private_endpoint_network_policies
-  private_link_service_network_policies_enabled = var.private_link_service_network_policies_enabled
-
-  service_endpoints = var.service_endpoints
+  name                                           = var.subnet_name
+  resource_group_name                            = var.core_resource_group_name
+  virtual_network_name                           = var.virtual_network_name
+  address_prefixes                               = var.subnet_address_prefixes
+  enforce_private_link_endpoint_network_policies = var.subnet_enforce_private_link_endpoint_network_policies
+  enforce_private_link_service_network_policies  = var.subnet_enforce_private_link_service_network_policies
+  service_endpoints                              = var.service_endpoints
 
   dynamic "delegation" {
     for_each = var.delegation
---
+++ versions.tf
@@ -0,0 +1,13 @@
+# Pin exact version of azurerm provider from 3.17.0 up to 4.x
+# This is to inform the pre-commit hook that this module must be using older version of the azurerm instead of latest
+# So that we do not get errors due ot deprecated properties and other issues that can only be resolved by azurerm upgrade
+# Simple upgrade is not possible because bunch of upstream repositories/pipelines that use this module are still on the old azurerm provider and upgrade in this module breaks them
+# This version file needs to be present both in module and tests directory because pre-commit hook will create tmp version lock files and complain in each place
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.17.0"
+    }
+  }
+}
