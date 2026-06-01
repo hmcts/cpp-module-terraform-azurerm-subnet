a072f839aeda1e46cf00da9bdd34a2176fb65d4e
Dawid Strozak <dawid.strozak@hmcts.net>
2026-06-01 14:58:19 +0100

    Fix comment

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@
 # This is to inform the pre-commit hook that this module must be using older version of the azurerm instead of latest
 # So that we do not get errors due ot deprecated properties and other issues that can only be resolved by azurerm upgrade
 # Simple upgrade is not possible because bunch of upstream repositories/pipelines that use this module are still on the old azurerm provider and upgrade in this module breaks them
-# This version file needs to be present both here and within the tests directory because pre-commit hook will create tmp version lock files and complain in each place
+# This version file needs to be present both in module and tests directory because pre-commit hook will create tmp version lock files and complain in each place
 terraform {
   required_providers {
     azurerm = {
